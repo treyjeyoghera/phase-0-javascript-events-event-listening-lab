7c57112dc17c050ecaeba38f078fec9f8731a452
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-02 19:57:31 +0300

    work

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input = document.getElementById("button");
+
 function addingEventListener() {
+  input.addEventListener("click", run);
+}
+
+addingEventListener();
+
+function run(event) {
+
+  alert("Button clicked!");
 }
